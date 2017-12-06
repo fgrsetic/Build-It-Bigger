07a16724c0cec065aa92d890f251c68577bbb3a1
Franjo Gršetić <fgrsetic@gmail.com>
2017-12-06 15:21:45 +0100

    Add java and androdi libraries

--- jokedisplay/src/main/java/com/udacity/gradle/jokedisplay/JokeActivity.java
+++ jokedisplay/src/main/java/com/udacity/gradle/jokedisplay/JokeActivity.java
@@ -1,28 +1,26 @@
-package com.udacity.gradle.builditbigger;
+package com.udacity.gradle.jokedisplay;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
-
-import com.udacity.gradle.jokes.Joker;
 
 
-public class MainActivity extends AppCompatActivity {
+public class JokeActivity extends AppCompatActivity {
+
+    public static String JOKE_KEY = "Joke key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_joke);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_joke, menu);
         return true;
     }
 
@@ -40,13 +38,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void tellJoke(View view) {
-        Joker joke = new Joker();
-        String jokeFromJavaLibrary = joke.getJoke();
-
-        Toast.makeText(this, jokeFromJavaLibrary, Toast.LENGTH_SHORT).show();
-    }
-
-
 }
